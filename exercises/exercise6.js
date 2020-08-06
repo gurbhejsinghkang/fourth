cc2b01c53bdff0866feb7ce15f6309a0f7d0e72d
The persistent2020 <gurbhejsinghkang@gmail.com>
2020-08-06 19:00:03 -0400

    third commit

---
+++ exercises/exercise6.js
@@ -0,0 +1,22 @@
+function ATM(accountno) {
+    var balance = 0;
+    return {
+        deposit: function (money) {
+            balance = balance + money;
+        },
+        withdrawl: function (money) {
+            if (balance <= 0) {
+                balance = "very low";
+            } else {
+                balance = balance - money;
+            }
+        },
+        getresult: function () {
+            return "account no " + accountno + "balance is " + balance;
+        }
+    }
+}
+
+var rel = ATM(100);
+rel.deposit(1000);
+console.log(rel.getresult());
