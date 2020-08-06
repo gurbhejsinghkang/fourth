fa64289ab47b0a89a94d74c75ea54d68bfa0ef2b
The persistent2020 <gurbhejsinghkang@gmail.com>
2020-08-06 19:02:30 -0400

    hdhdh

--- exercises/exercise6.js
+++ exercises/exercise6.js
@@ -1,22 +1,23 @@
-function ATM(accountno) {
-    var balance = 0;
-    return {
-        deposit: function (money) {
-            balance = balance + money;
-        },
-        withdrawl: function (money) {
-            if (balance <= 0) {
-                balance = "very low";
-            } else {
-                balance = balance - money;
-            }
-        },
-        getresult: function () {
-            return "account no " + accountno + "balance is " + balance;
-        }
-    }
-}
+console.log("hello");
+// function ATM(accountno) {
+//     var balance = 0;
+//     return {
+//         deposit: function (money) {
+//             balance = balance + money;
+//         },
+//         withdrawl: function (money) {
+//             if (balance <= 0) {
+//                 balance = "very low";
+//             } else {
+//                 balance = balance - money;
+//             }
+//         },
+//         getresult: function () {
+//             return "account no " + accountno + "balance is " + balance;
+//         }
+//     }
+// }
 
-var rel = ATM(100);
-rel.deposit(1000);
-console.log(rel.getresult());
+// var rel = ATM(100);
+// rel.deposit(1000);
+// console.log(rel.getresult());
